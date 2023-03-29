0fb4db0c21f3d8999b40d945043fd62c808c93b5
tygr <graytd1996@gmail.com>
2023-03-29 19:22:46 -0400

    Fix extra character in cipher method

--- cipher.rb
+++ cipher.rb
@@ -1,8 +1,9 @@
 # ceasar cipher
+
 def cipher(string,shift)
   new_string = string.split('').map do |char|
     if (char.ord > 65 && char.ord + shift < 90) || (char.ord > 96 && char.ord + shift < 122)
-      (char.ord + shift_).chr
+      (char.ord + shift).chr
     elsif (char.ord > 65 && char.ord + shift > 90) || (char.ord > 96 && char.ord + shift > 122)
       (char.ord + shift - 26).chr
     else
