566ad8cacf11203dcf62dd21b0ef7bfe693c4f82
tygr <graytd1996@gmail.com>
2023-03-29 18:58:48 -0400

    Add my cipher project

---
+++ cipher.rb
@@ -0,0 +1,14 @@
+# ceasar cipher
+def cipher(string,shift)
+  new_string = string.split('').map do |char|
+    if (char.ord > 65 && char.ord + shift < 90) || (char.ord > 96 && char.ord + shift < 122)
+      (char.ord + shift_).chr
+    elsif (char.ord > 65 && char.ord + shift > 90) || (char.ord > 96 && char.ord + shift > 122)
+      (char.ord + shift - 26).chr
+    else
+      char
+    end
+  end
+  new_string.join
+end
+p cipher('What a string!', 5)
